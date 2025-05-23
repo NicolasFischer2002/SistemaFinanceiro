7ee22e478446b529085b9205883ec9d0de15af65
Gustavo Firmino de Oliveira <gustavo.firmino.oli@gmail.com>
2025-05-23 00:45:32 -0300

    Commit

--- Apresentacao/FormDespesas.Designer.cs
+++ Apresentacao/FormDespesas.Designer.cs
@@ -28,8 +28,9 @@
         /// </summary>
         private void InitializeComponent()
         {
-            label1 = new Label();
             panel1 = new Panel();
+            NumeroParcelas = new NumericUpDown();
+            label1 = new Label();
             radioButtonDespesaEmAberto = new RadioButton();
             radioButtonDespesaQuitada = new RadioButton();
             btnInserirDespesa = new Button();
@@ -43,27 +44,19 @@
             CbBoxStatus = new ComboBox();
             label4 = new Label();
             label8 = new Label();
-            TxtBoxNumeroParcelas = new TextBox();
             TxtBoxValor = new TextBox();
             label3 = new Label();
             label2 = new Label();
             TxtBoxDescricao = new TextBox();
             panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)NumeroParcelas).BeginInit();
             SuspendLayout();
-            // 
-            // label1
-            // 
-            label1.AutoSize = true;
-            label1.Font = new Font("Segoe UI", 18F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            label1.Location = new Point(12, 9);
-            label1.Name = "label1";
-            label1.Size = new Size(242, 32);
-            label1.TabIndex = 2;
-            label1.Text = "Cadastre sua despesa";
             // 
             // panel1
             // 
             panel1.Anchor = AnchorStyles.None;
+            panel1.Controls.Add(NumeroParcelas);
+            panel1.Controls.Add(label1);
             panel1.Controls.Add(radioButtonDespesaEmAberto);
             panel1.Controls.Add(radioButtonDespesaQuitada);
             panel1.Controls.Add(btnInserirDespesa);
@@ -77,21 +70,38 @@
             panel1.Controls.Add(CbBoxStatus);
             panel1.Controls.Add(label4);
             panel1.Controls.Add(label8);
-            panel1.Controls.Add(TxtBoxNumeroParcelas);
             panel1.Controls.Add(TxtBoxValor);
             panel1.Controls.Add(label3);
             panel1.Controls.Add(label2);
             panel1.Controls.Add(TxtBoxDescricao);
-            panel1.Location = new Point(224, 26);
+            panel1.Location = new Point(220, 26);
             panel1.Name = "panel1";
-            panel1.Size = new Size(941, 430);
+            panel1.Size = new Size(941, 520);
             panel1.TabIndex = 3;
+            // 
+            // NumeroParcelas
+            // 
+            NumeroParcelas.Font = new Font("Segoe UI", 16F);
+            NumeroParcelas.Location = new Point(269, 108);
+            NumeroParcelas.Name = "NumeroParcelas";
+            NumeroParcelas.Size = new Size(120, 36);
+            NumeroParcelas.TabIndex = 33;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 18F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label1.Location = new Point(49, 19);
+            label1.Name = "label1";
+            label1.Size = new Size(279, 32);
+            label1.TabIndex = 32;
+            label1.Text = "CADASTRE SUA DESPESA";
             // 
             // radioButtonDespesaEmAberto
             // 
             radioButtonDespesaEmAberto.AutoSize = true;
             radioButtonDespesaEmAberto.Font = new Font("Segoe UI", 16F);
-            radioButtonDespesaEmAberto.Location = new Point(681, 182);
+            radioButtonDespesaEmAberto.Location = new Point(681, 243);
             radioButtonDespesaEmAberto.Name = "radioButtonDespesaEmAberto";
             radioButtonDespesaEmAberto.Size = new Size(210, 34);
             radioButtonDespesaEmAberto.TabIndex = 27;
@@ -104,7 +114,7 @@
             // 
             radioButtonDespesaQuitada.AutoSize = true;
             radioButtonDespesaQuitada.Font = new Font("Segoe UI", 16F);
-            radioButtonDespesaQuitada.Location = new Point(435, 182);
+            radioButtonDespesaQuitada.Location = new Point(435, 243);
             radioButtonDespesaQuitada.Name = "radioButtonDespesaQuitada";
             radioButtonDespesaQuitada.Size = new Size(181, 34);
             radioButtonDespesaQuitada.TabIndex = 26;
@@ -115,11 +125,12 @@
             // 
             // btnInserirDespesa
             // 
-            btnInserirDespesa.Location = new Point(339, 364);
+            btnInserirDespesa.Font = new Font("Segoe UI", 16F);
+            btnInserirDespesa.Location = new Point(337, 425);
             btnInserirDespesa.Name = "btnInserirDespesa";
-            btnInserirDespesa.Size = new Size(255, 49);
+            btnInserirDespesa.Size = new Size(300, 70);
             btnInserirDespesa.TabIndex = 30;
-            btnInserirDespesa.Text = "Cadastrar";
+            btnInserirDespesa.Text = "CADASTRAR";
             btnInserirDespesa.UseVisualStyleBackColor = true;
             btnInserirDespesa.Click += btnInserirDespesa_Click;
             // 
@@ -127,7 +138,7 @@
             // 
             CbBoxTipo.Font = new Font("Segoe UI", 16F);
             CbBoxTipo.FormattingEnabled = true;
-            CbBoxTipo.Location = new Point(49, 219);
+            CbBoxTipo.Location = new Point(49, 280);
             CbBoxTipo.Name = "CbBoxTipo";
             CbBoxTipo.Size = new Size(310, 38);
             CbBoxTipo.TabIndex = 25;
@@ -136,7 +147,7 @@
             // 
             CbBoxCategoria.Font = new Font("Segoe UI", 16F);
             CbBoxCategoria.FormattingEnabled = true;
-            CbBoxCategoria.Location = new Point(49, 134);
+            CbBoxCategoria.Location = new Point(49, 195);
             CbBoxCategoria.Name = "CbBoxCategoria";
             CbBoxCategoria.Size = new Size(310, 38);
             CbBoxCategoria.TabIndex = 23;
@@ -145,7 +156,7 @@
             // 
             label6.AutoSize = true;
             label6.Font = new Font("Segoe UI", 16F);
-            label6.Location = new Point(49, 101);
+            label6.Location = new Point(49, 162);
             label6.Name = "label6";
             label6.Size = new Size(107, 30);
             label6.TabIndex = 25;
@@ -155,7 +166,7 @@
             // 
             label9.AutoSize = true;
             label9.Font = new Font("Segoe UI", 16F);
-            label9.Location = new Point(49, 186);
+            label9.Location = new Point(49, 247);
             label9.Name = "label9";
             label9.Size = new Size(56, 30);
             label9.TabIndex = 31;
@@ -165,7 +176,7 @@
             // 
             dateTimePickerPagamento.CalendarFont = new Font("Segoe UI", 17F);
             dateTimePickerPagamento.Font = new Font("Segoe UI", 17F);
-            dateTimePickerPagamento.Location = new Point(435, 219);
+            dateTimePickerPagamento.Location = new Point(435, 280);
             dateTimePickerPagamento.Name = "dateTimePickerPagamento";
             dateTimePickerPagamento.Size = new Size(456, 38);
             dateTimePickerPagamento.TabIndex = 28;
@@ -174,7 +185,7 @@
             // 
             label7.AutoSize = true;
             label7.Font = new Font("Segoe UI", 16F);
-            label7.Location = new Point(435, 100);
+            label7.Location = new Point(435, 161);
             label7.Name = "label7";
             label7.Size = new Size(176, 30);
             label7.TabIndex = 28;
@@ -184,7 +195,7 @@
             // 
             dateTimePickerVencimento.CalendarFont = new Font("Segoe UI", 17F);
             dateTimePickerVencimento.Font = new Font("Segoe UI", 17F);
-            dateTimePickerVencimento.Location = new Point(435, 134);
+            dateTimePickerVencimento.Location = new Point(435, 195);
             dateTimePickerVencimento.Name = "dateTimePickerVencimento";
             dateTimePickerVencimento.Size = new Size(456, 38);
             dateTimePickerVencimento.TabIndex = 24;
@@ -193,7 +204,7 @@
             // 
             CbBoxStatus.Font = new Font("Segoe UI", 16F);
             CbBoxStatus.FormattingEnabled = true;
-            CbBoxStatus.Location = new Point(435, 47);
+            CbBoxStatus.Location = new Point(435, 108);
             CbBoxStatus.Name = "CbBoxStatus";
             CbBoxStatus.Size = new Size(456, 38);
             CbBoxStatus.TabIndex = 22;
@@ -202,7 +213,7 @@
             // 
             label4.AutoSize = true;
             label4.Font = new Font("Segoe UI", 16F);
-            label4.Location = new Point(435, 14);
+            label4.Location = new Point(435, 75);
             label4.Name = "label4";
             label4.Size = new Size(70, 30);
             label4.TabIndex = 23;
@@ -212,25 +223,16 @@
             // 
             label8.AutoSize = true;
             label8.Font = new Font("Segoe UI", 16F);
-            label8.Location = new Point(269, 14);
+            label8.Location = new Point(269, 75);
             label8.Name = "label8";
             label8.Size = new Size(90, 30);
             label8.TabIndex = 22;
             label8.Text = "Parcelas";
             // 
-            // TxtBoxNumeroParcelas
-            // 
-            TxtBoxNumeroParcelas.Font = new Font("Segoe UI", 16F);
-            TxtBoxNumeroParcelas.Location = new Point(269, 47);
-            TxtBoxNumeroParcelas.Name = "TxtBoxNumeroParcelas";
-            TxtBoxNumeroParcelas.Size = new Size(90, 36);
-            TxtBoxNumeroParcelas.TabIndex = 21;
-            TxtBoxNumeroParcelas.TextChanged += NumeroParcelas_TextChanged;
-            // 
             // TxtBoxValor
             // 
             TxtBoxValor.Font = new Font("Segoe UI", 16F);
-            TxtBoxValor.Location = new Point(49, 47);
+            TxtBoxValor.Location = new Point(49, 108);
             TxtBoxValor.Name = "TxtBoxValor";
             TxtBoxValor.Size = new Size(193, 36);
             TxtBoxValor.TabIndex = 20;
@@ -239,7 +241,7 @@
             // 
             label3.AutoSize = true;
             label3.Font = new Font("Segoe UI", 16F);
-            label3.Location = new Point(49, 14);
+            label3.Location = new Point(49, 75);
             label3.Name = "label3";
             label3.Size = new Size(62, 30);
             label3.TabIndex = 19;
@@ -249,7 +251,7 @@
             // 
             label2.AutoSize = true;
             label2.Font = new Font("Segoe UI", 16F);
-            label2.Location = new Point(49, 273);
+            label2.Location = new Point(49, 334);
             label2.Name = "label2";
             label2.Size = new Size(106, 30);
             label2.TabIndex = 3;
@@ -258,7 +260,7 @@
             // TxtBoxDescricao
             // 
             TxtBoxDescricao.Font = new Font("Segoe UI", 16F);
-            TxtBoxDescricao.Location = new Point(49, 306);
+            TxtBoxDescricao.Location = new Point(49, 367);
             TxtBoxDescricao.Name = "TxtBoxDescricao";
             TxtBoxDescricao.Size = new Size(842, 36);
             TxtBoxDescricao.TabIndex = 29;
@@ -268,26 +270,22 @@
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.Cyan;
-            ClientSize = new Size(1379, 685);
+            ClientSize = new Size(1370, 685);
             Controls.Add(panel1);
-            Controls.Add(label1);
             Name = "FormDespesas";
             Text = "FormDespesas";
             Load += FormDespesas_Load;
             panel1.ResumeLayout(false);
             panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)NumeroParcelas).EndInit();
             ResumeLayout(false);
-            PerformLayout();
         }
 
         #endregion
-
-        private Label label1;
         private Panel panel1;
         private TextBox TxtBoxDescricao;
         private Label label2;
         private Label label8;
-        private TextBox TxtBoxNumeroParcelas;
         private TextBox TxtBoxValor;
         private Label label3;
         private ComboBox CbBoxStatus;
@@ -302,5 +300,7 @@
         private Button btnInserirDespesa;
         private RadioButton radioButtonDespesaQuitada;
         private RadioButton radioButtonDespesaEmAberto;
+        private Label label1;
+        private NumericUpDown NumeroParcelas;
     }
 }
--- Apresentacao/FormReceitas.Designer.cs
+++ Apresentacao/FormReceitas.Designer.cs
@@ -29,30 +29,34 @@
         private void InitializeComponent()
         {
             panel1 = new Panel();
+            NumeroParcelas = new NumericUpDown();
+            NumeroParcelas_ = new TextBox();
             label8 = new Label();
-            NumeroParcelas = new TextBox();
             label1 = new Label();
             button1 = new Button();
             label7 = new Label();
             dateTimePicker = new DateTimePicker();
             CbBoxCategoria = new ComboBox();
+            CbBoxTipo = new ComboBox();
             CbBoxStatus = new ComboBox();
             label6 = new Label();
+            label5 = new Label();
             label4 = new Label();
             TxtBoxValor = new TextBox();
             label3 = new Label();
             TxtBoxDescricao = new TextBox();
             label2 = new Label();
-            label5 = new Label();
-            CbBoxTipo = new ComboBox();
             panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)NumeroParcelas).BeginInit();
             SuspendLayout();
             // 
             // panel1
             // 
+            panel1.Anchor = AnchorStyles.None;
             panel1.BackColor = SystemColors.ScrollBar;
-            panel1.Controls.Add(label8);
             panel1.Controls.Add(NumeroParcelas);
+            panel1.Controls.Add(NumeroParcelas_);
+            panel1.Controls.Add(label8);
             panel1.Controls.Add(label1);
             panel1.Controls.Add(button1);
             panel1.Controls.Add(label7);
@@ -68,28 +72,37 @@
             panel1.Controls.Add(TxtBoxDescricao);
             panel1.Controls.Add(label2);
             panel1.Font = new Font("Segoe UI", 16F);
-            panel1.Location = new Point(388, 12);
+            panel1.Location = new Point(224, 26);
             panel1.Name = "panel1";
-            panel1.Size = new Size(643, 661);
+            panel1.Size = new Size(941, 520);
             panel1.TabIndex = 2;
+            // 
+            // NumeroParcelas
+            // 
+            NumeroParcelas.Location = new Point(407, 117);
+            NumeroParcelas.Name = "NumeroParcelas";
+            NumeroParcelas.Size = new Size(90, 36);
+            NumeroParcelas.TabIndex = 3;
+            // 
+            // NumeroParcelas_
+            // 
+            NumeroParcelas_.Enabled = false;
+            NumeroParcelas_.Font = new Font("Segoe UI", 16F);
+            NumeroParcelas_.Location = new Point(-105, 432);
+            NumeroParcelas_.Name = "NumeroParcelas_";
+            NumeroParcelas_.Size = new Size(190, 36);
+            NumeroParcelas_.TabIndex = 3;
+            NumeroParcelas_.Visible = false;
             // 
             // label8
             // 
             label8.AutoSize = true;
             label8.Font = new Font("Segoe UI", 16F);
-            label8.Location = new Point(237, 194);
+            label8.Location = new Point(407, 84);
             label8.Name = "label8";
             label8.Size = new Size(90, 30);
             label8.TabIndex = 18;
             label8.Text = "Parcelas";
-            // 
-            // NumeroParcelas
-            // 
-            NumeroParcelas.Font = new Font("Segoe UI", 16F);
-            NumeroParcelas.Location = new Point(237, 227);
-            NumeroParcelas.Name = "NumeroParcelas";
-            NumeroParcelas.Size = new Size(190, 36);
-            NumeroParcelas.TabIndex = 17;
             // 
             // label1
             // 
@@ -97,17 +110,18 @@
             label1.Font = new Font("Segoe UI", 18F, FontStyle.Regular, GraphicsUnit.Point, 0);
             label1.Location = new Point(15, 19);
             label1.Name = "label1";
-            label1.Size = new Size(171, 32);
+            label1.Size = new Size(269, 32);
             label1.TabIndex = 16;
-            label1.Text = "TELA RECEITAS";
+            label1.Text = "CADASTRE SUA RECEITA";
             // 
             // button1
             // 
-            button1.Location = new Point(175, 599);
+            button1.Anchor = AnchorStyles.None;
+            button1.Location = new Point(332, 432);
             button1.Name = "button1";
-            button1.Size = new Size(320, 49);
-            button1.TabIndex = 15;
-            button1.Text = "INSERIR";
+            button1.Size = new Size(300, 70);
+            button1.TabIndex = 8;
+            button1.Text = "CADASTRAR";
             button1.UseVisualStyleBackColor = true;
             button1.Click += button1_Click;
             // 
@@ -115,7 +129,7 @@
             // 
             label7.AutoSize = true;
             label7.Font = new Font("Segoe UI", 16F);
-            label7.Location = new Point(175, 501);
+            label7.Location = new Point(175, 247);
             label7.Name = "label7";
             label7.Size = new Size(57, 30);
             label7.TabIndex = 14;
@@ -123,45 +137,64 @@
             // 
             // dateTimePicker
             // 
-            dateTimePicker.Font = new Font("Segoe UI", 14F);
-            dateTimePicker.Location = new Point(175, 543);
+            dateTimePicker.Font = new Font("Segoe UI", 16F);
+            dateTimePicker.Location = new Point(167, 276);
             dateTimePicker.Name = "dateTimePicker";
-            dateTimePicker.Size = new Size(320, 32);
-            dateTimePicker.TabIndex = 13;
+            dateTimePicker.Size = new Size(376, 36);
+            dateTimePicker.TabIndex = 7;
             // 
             // CbBoxCategoria
             // 
             CbBoxCategoria.Font = new Font("Segoe UI", 16F);
             CbBoxCategoria.FormattingEnabled = true;
-            CbBoxCategoria.Location = new Point(237, 460);
+            CbBoxCategoria.Location = new Point(562, 206);
             CbBoxCategoria.Name = "CbBoxCategoria";
-            CbBoxCategoria.Size = new Size(190, 38);
-            CbBoxCategoria.TabIndex = 12;
+            CbBoxCategoria.Size = new Size(223, 38);
+            CbBoxCategoria.TabIndex = 6;
+            // 
+            // CbBoxTipo
+            // 
+            CbBoxTipo.Font = new Font("Segoe UI", 16F);
+            CbBoxTipo.FormattingEnabled = true;
+            CbBoxTipo.Location = new Point(503, 115);
+            CbBoxTipo.Name = "CbBoxTipo";
+            CbBoxTipo.Size = new Size(211, 38);
+            CbBoxTipo.TabIndex = 5;
             // 
             // CbBoxStatus
             // 
             CbBoxStatus.Font = new Font("Segoe UI", 16F);
             CbBoxStatus.FormattingEnabled = true;
-            CbBoxStatus.Location = new Point(237, 305);
+            CbBoxStatus.Location = new Point(175, 206);
             CbBoxStatus.Name = "CbBoxStatus";
-            CbBoxStatus.Size = new Size(193, 38);
-            CbBoxStatus.TabIndex = 10;
+            CbBoxStatus.Size = new Size(381, 38);
+            CbBoxStatus.TabIndex = 4;
             // 
             // label6
             // 
             label6.AutoSize = true;
             label6.Font = new Font("Segoe UI", 16F);
-            label6.Location = new Point(237, 427);
+            label6.Location = new Point(562, 173);
             label6.Name = "label6";
             label6.Size = new Size(107, 30);
             label6.TabIndex = 8;
             label6.Text = "Categoria";
             // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Segoe UI", 16F);
+            label5.Location = new Point(503, 82);
+            label5.Name = "label5";
+            label5.Size = new Size(56, 30);
+            label5.TabIndex = 6;
+            label5.Text = "Tipo";
+            // 
             // label4
             // 
             label4.AutoSize = true;
             label4.Font = new Font("Segoe UI", 16F);
-            label4.Location = new Point(237, 269);
+            label4.Location = new Point(175, 173);
             label4.Name = "label4";
             label4.Size = new Size(70, 30);
             label4.TabIndex = 4;
@@ -170,16 +203,16 @@
             // TxtBoxValor
             // 
             TxtBoxValor.Font = new Font("Segoe UI", 16F);
-            TxtBoxValor.Location = new Point(237, 156);
+            TxtBoxValor.Location = new Point(175, 117);
             TxtBoxValor.Name = "TxtBoxValor";
-            TxtBoxValor.Size = new Size(190, 36);
-            TxtBoxValor.TabIndex = 3;
+            TxtBoxValor.Size = new Size(223, 36);
+            TxtBoxValor.TabIndex = 2;
             // 
             // label3
             // 
             label3.AutoSize = true;
             label3.Font = new Font("Segoe UI", 16F);
-            label3.Location = new Point(237, 123);
+            label3.Location = new Point(175, 84);
             label3.Name = "label3";
             label3.Size = new Size(62, 30);
             label3.TabIndex = 2;
@@ -188,39 +221,20 @@
             // TxtBoxDescricao
             // 
             TxtBoxDescricao.Font = new Font("Segoe UI", 16F);
-            TxtBoxDescricao.Location = new Point(237, 75);
+            TxtBoxDescricao.Location = new Point(175, 356);
             TxtBoxDescricao.Name = "TxtBoxDescricao";
-            TxtBoxDescricao.Size = new Size(190, 36);
+            TxtBoxDescricao.Size = new Size(610, 36);
             TxtBoxDescricao.TabIndex = 1;
             // 
             // label2
             // 
             label2.AutoSize = true;
             label2.Font = new Font("Segoe UI", 16F);
-            label2.Location = new Point(237, 42);
+            label2.Location = new Point(175, 323);
             label2.Name = "label2";
             label2.Size = new Size(106, 30);
             label2.TabIndex = 0;
             label2.Text = "Descrição";
-            // 
-            // label5
-            // 
-            label5.AutoSize = true;
-            label5.Font = new Font("Segoe UI", 16F);
-            label5.Location = new Point(237, 349);
-            label5.Name = "label5";
-            label5.Size = new Size(56, 30);
-            label5.TabIndex = 6;
-            label5.Text = "Tipo";
-            // 
-            // CbBoxTipo
-            // 
-            CbBoxTipo.Font = new Font("Segoe UI", 16F);
-            CbBoxTipo.FormattingEnabled = true;
-            CbBoxTipo.Location = new Point(237, 382);
-            CbBoxTipo.Name = "CbBoxTipo";
-            CbBoxTipo.Size = new Size(190, 38);
-            CbBoxTipo.TabIndex = 11;
             // 
             // FormReceitas
             // 
@@ -234,6 +248,7 @@
             Load += FormReceitas_Load;
             panel1.ResumeLayout(false);
             panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)NumeroParcelas).EndInit();
             ResumeLayout(false);
         }
 
@@ -251,9 +266,10 @@
         private Label label2;
         private Button button1;
         private Label label1;
-        private TextBox NumeroParcelas;
+        private TextBox NumeroParcelas_;
         private Label label8;
         private ComboBox CbBoxTipo;
         private Label label5;
+        private NumericUpDown NumeroParcelas;
     }
 }
